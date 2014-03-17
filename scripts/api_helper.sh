448c30d3d49b3805359a63dcf40f3d331ce21c42
Craig Cabrey <craigcabrey@gmail.com>
2014-03-16 23:49:04 -0400

    Added api helper script for testing changes

---
+++ scripts/api_helper.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+URI=${URI:='https'}
+URL=${URL:=127.0.0.1}
+PORT=${PORT:=3000}
+
+BASE=$URI://$URL:$PORT
+
+function register() {
+  curl -X POST -k -d "type=$2&name=$3&email=$4&password=$5&postal=$6" $BASE/users/register
+}
+
+function login() {
+  curl -X POST -k -d "email=$2&password=$3" $BASE/users/login
+}
+
+function logout() {
+  curl -X POST -k -d "email=$2&token=$3" $BASE/users/logout
+}
+
+function send-message() {
+  curl -X POST -k -d "email=$2&token=$3&userId=$4&contents=$5" $BASE/messages/send
+}
+
+function get-messages() {
+  curl -X GET -k -d "email=$2&token=$3" $BASE/messages
+}
+
+function help() {
+  echo -e "Available commands (and usage):";
+  echo -e "\t* register:\t type name email password postal";
+  echo -e "\t* login:\t email password";
+  echo -e "\t* logout:\t email token";
+  echo -e "\t* send-message:\t userID message";
+  echo -e "\t* get-messages:\t No arguments";
+  exit 1
+}
+
+case $1 in
+  register)
+    if [ $# -ne 6 ]
+    then
+      help
+    else
+      register $@
+    fi
+    ;;
+  login)
+    if [ $# -ne 3 ]
+    then
+      help
+    else
+      login $@
+    fi
+    ;;
+  logout)
+    if [ $# -ne 3 ]
+    then
+      help
+    else
+      logout $@
+    fi
+    ;;
+  send-message)
+    if [ $# -ne 3 ]
+    then
+      help
+    else
+      send-message $@
+    fi
+    ;;
+  get-messages)
+    if [ $# -ne 2 ]
+    then
+      help
+    else
+      get-messages
+    fi
+    ;;
+  *)
+    help
+    ;;
+esac
+
+echo ""
+
