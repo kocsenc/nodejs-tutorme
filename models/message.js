ab02ababf4c1fdfd28b94f4b3594ec56a06c6e82
Craig Cabrey <craigcabrey@gmail.com>
2014-03-20 16:37:03 -0400

    Add read field to message model

--- models/message.js
+++ models/message.js
@@ -1,6 +1,9 @@
 // Message Model
 module.exports = function(sequelize, DataTypes) {
   var Message = sequelize.define('Message', {
+    read: {
+      type: DataTypes.BOOLEAN
+    },
     contents: {
       type: DataTypes.TEXT,
       allowNull: false
